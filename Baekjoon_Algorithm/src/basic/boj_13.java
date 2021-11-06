96e86116f11800e1dedc452dc65295c2a54574e6
dohyun <dohyun1225@gmail.com>
2021-11-06 21:47:54 +0900

    boj_13

---
+++ Baekjoon_Algorithm/src/basic/boj_13.java
@@ -0,0 +1,48 @@
+package basic;
+
+import java.util.Scanner;
+
+public class boj_13 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//백준 4344번 평균은 넘겠지
+		
+		/*	각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 
+			소수점 셋째 자리까지 출력한다.*/
+		
+		//평균, 평균을넘는학생&평균                                                                            
+		
+		int c = sc.nextInt(); //케이스 수
+		int n = 0; //학생 수
+		double avg = 0; //평균
+		
+		int arr[] = new int[10];
+		
+		for(int i=0; i< c; i++) { //1.케이스 수만큼 
+			n = sc.nextInt(); //학생 수
+			int total = 0;
+			int num= 0;
+			
+			for(int k=0; k<n; k++) { //2.학생 수만큼 
+				arr[k] = sc.nextInt(); //각 학생의 점수
+				total += arr[k]; //합계구하기
+			}
+			
+			avg = (double) total / n; //평균구하기
+			
+			for(int k=0; k<n; k++) {//3.평균보다 높은 학생 수 구하기 
+					if(arr[k] > avg) { 
+						num++;
+					}
+			}
+			System.out.printf("%.3f", 100.0 * num / n);
+			System.out.println("%");
+		}
+		
+		
+		
+		
+		
+	}
+}
